af556c0fcd9854969f7c08fce6779a94dc9e26aa
Alina Melenteva <almelenteva@yandex.ru>
2017-12-18 18:50:28 +0300

    proba

---
+++ program.cpp
@@ -0,0 +1,20 @@
+//פאיכ program.cpp
+#include <iostream>
+#include "funcs.h"
+using namespace std;
+
+int main()
+{
+<<<<<<< HEAD
+		cout << MySub(10, 3) << endl; 
+=======
+	cout << MySum(10, 3) << endl;
+	system("pause");
+return 0;
+
+>>>>>>> 33114046a0a3ec416c9699ab3049f8f495222725
+}
+
+
+
+
